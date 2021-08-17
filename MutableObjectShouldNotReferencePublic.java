5b99f07b8010daab92a098db14b5637cf1e981ab
spyd3r98 <54056259+spyd3r98@users.noreply.github.com>
2021-08-17 14:16:08 +0530

    Update MutableObjectShouldNotReferencePublic.java

--- MutableObjectShouldNotReferencePublic.java
+++ MutableObjectShouldNotReferencePublic.java
@@ -4,5 +4,9 @@ import java.util.*;
 public class MutableObjectShouldNotReferencePublic {
     public static final String[] a = {"green","yellow","red","orange","purple"};
     
+    public static final String arr[]={"Hello","My","Name","is"};
+  
+    public static final int[] arr2={1,2,3,4,5,56};
+    
     public static final String b = "String";
 }
