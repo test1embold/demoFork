4c1e5b29ce7f251edb3aff1dba15b362d34b4dee
spyd3r98 <54056259+spyd3r98@users.noreply.github.com>
2021-09-22 16:43:06 +0530

    Create publicStaticFieldShouldBeFinal.java

---
+++ publicStaticFieldShouldBeFinal.java
@@ -0,0 +1,9 @@
+package javacodechecker;
+
+public class publicStaticFieldShouldBeFinal{
+  public static String HELLO = "Hello my name is zuzzi";
+  
+  public static String BYE = "Bye bye friends";
+  
+  public static final String ARA_ARA = "ara ara gome";
+}
