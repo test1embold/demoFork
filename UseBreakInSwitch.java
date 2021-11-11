90351dadf01290446cac816404f39683971ee54f
spyd3r98 <54056259+spyd3r98@users.noreply.github.com>
2021-11-11 11:39:49 +0530

    Create UseBreakInSwitch.java

---
+++ UseBreakInSwitch.java
@@ -0,0 +1,20 @@
+package demo;
+public class UseBreakInSwitch {
+
+    public void Statement(){
+        int day = 2;
+        switch (day){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+            case 3:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("Default");
+
+        }
+    }
+}
