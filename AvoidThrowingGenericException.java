8975d5673059e5f0aa155f043862cdf99f2043c6
spyd3r98 <54056259+spyd3r98@users.noreply.github.com>
2021-11-12 11:12:48 +0530

    Create AvoidThrowingGenericException.java

---
+++ AvoidThrowingGenericException.java
@@ -0,0 +1,21 @@
+package javacodechecker;
+
+public class AvoidThrowingGenericException {
+    
+    public void M1()throws Exception{
+        logger.info("Throwing generic exception");
+    }
+    
+    public void M2()throws Throwable{
+        logger.info("Throwing generic exception");
+    }
+    
+    public void M3()throws  InvocationTargetException{
+        logger.info("Throwing InvocationTargetException");
+    }
+
+   
+    public void M5()throws IOException, InvocationTargetException, SQLException{
+        logger.info("Throwing IOException, InvocationTargetException, SQLException");
+    }
+}
