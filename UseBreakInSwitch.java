15962e859ebe150d386b100745908cdec5b099ae
spyd3r98 <54056259+spyd3r98@users.noreply.github.com>
2021-11-15 17:23:45 +0530

    Update UseBreakInSwitch.java

--- UseBreakInSwitch.java
+++ UseBreakInSwitch.java
@@ -4,17 +4,22 @@ public class UseBreakInSwitch {
     public void Statement(){
         int day = 2;
         int n =2;
+       
         switch (day){
+
             case 1:
-                System.out.println("1");
+                logger.info("1");
                 break;
+
             case 2:
-                System.out.println("2");
+                logger.info("2");
+
             case 3:
-                System.out.println("3");
+                logger.info("3");
                 break;
+
             default:
-                System.out.println("Default");
+                logger.info("4");
 
         }
         
@@ -22,7 +27,7 @@ public class UseBreakInSwitch {
 
             case 1:
                 logger.info("1");
-               
+                break;
 
             case 2:
                 logger.info("2");
@@ -31,5 +36,69 @@ public class UseBreakInSwitch {
                 logger.info("4");
 
         }
+       
+        switch (n){
+
+            case 1:
+
+            case 2:
+                logger.info("2");
+
+        }
+        
+        switch (n){
+
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                logger.info("2");
+                break;
+
+            default:
+                logger.info("4");
+
+        }
+        
+        switch (n){
+
+            default:
+                logger.info("4");
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                logger.info("2");
+                break;
+
+        }
+        
+        switch (n){
+
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                logger.info("2");
+                break;
+
+            default:
+                logger.info("4");
+
+        }
+        
+        switch (n){
+
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                logger.info("2");
+
+            default:
+                logger.info("4");
+
+        }
+
     }
 }
