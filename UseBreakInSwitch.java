fa1128dc1c200024997c308eb946130d44fe88dd
spyd3r98 <54056259+spyd3r98@users.noreply.github.com>
2021-11-15 14:20:24 +0530

    Update UseBreakInSwitch.java

--- UseBreakInSwitch.java
+++ UseBreakInSwitch.java
@@ -3,6 +3,7 @@ public class UseBreakInSwitch {
 
     public void Statement(){
         int day = 2;
+        int n =2;
         switch (day){
             case 1:
                 System.out.println("1");
@@ -14,6 +15,20 @@ public class UseBreakInSwitch {
                 break;
             default:
                 System.out.println("Default");
+
+        }
+        
+        switch (n){
+
+            case 1:
+                logger.info("1");
+                break;
+
+            case 2:
+                logger.info("2");
+
+            default:
+                logger.info("4");
 
         }
     }
