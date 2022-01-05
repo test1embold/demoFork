63568de970d693749a545c4ae4740a22937e299a
Manasi1305 <77058091+Manasi1305@users.noreply.github.com>
2022-01-05 10:57:47 +0530

    Create InsufficientlyProtectedCredentials.java

---
+++ InsufficientlyProtectedCredentials.java
@@ -0,0 +1,6 @@
+public class InsufficientlyProtectedCredentials{
+Properties prop = new Properties();
+prop.load(new FileInputStream("config.properties"));
+String password = prop.getProperty("password");
+DriverManager.getConnection(url, usr, password);
+}
